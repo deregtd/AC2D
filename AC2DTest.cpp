9f3a9bd111937daf72006c861fc6646f0883cfc1
Steven T. Snyder <ssnyder@appliedminds.com>
2019-07-20 19:07:55 -0700

    Message box at startup no longer says 'Error' since it's just sharing the command line parameters

--- AC2DTest.cpp
+++ AC2DTest.cpp
@@ -19,7 +19,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
                      LPTSTR    lpCmdLine,
                      int       nCmdShow)
 {
-	MessageBox(NULL, lpCmdLine, NULL, MB_OK);
+	MessageBox(NULL, lpCmdLine, "Commmand Line Parameters:", MB_OK);
 
 	hInst = hInstance;
 
